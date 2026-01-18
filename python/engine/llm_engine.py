68a02dd383c6846b85d993028f8795f7c14e0e69
Priyadarshan Garg <gargpriyadarshan18@gmail.com>
2026-01-18 15:32:54 +0530

    Made LLM engine the core brain for processing every command usign llama 3.1 latest

---
+++ python/engine/llm_engine.py
@@ -0,0 +1,18 @@
+
+import ollama
+
+class LLM_Engine:
+    def __init__(self):
+        self.history = [ {"role": "system", "content": "You are Sarah, a highly advanced AI. Keep answers short, witty, and helpful."}
+            ]
+    def chat(self,text) :
+        self.history.append({"role": "user", "content": text})
+        response = ollama.chat(model= 'llama3.1', messages=self.history)
+        reply = response['message']['content']
+        self.history.append({"role": "assistant", "content": reply})
+        return reply
+
+
+
+
+
